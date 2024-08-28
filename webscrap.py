503a19ed4448f1fdf879cdd872a1ac9427aad090
João Ferreira <jo4o.fds@gmail.com>
2024-08-28 14:54:20 -0300

    Update webscrap.py

--- webscrap.py
+++ webscrap.py
@@ -1,4 +1,4 @@
-import requests
+import requests, os
 from bs4 import BeautifulSoup
 
 # URL do site que você deseja extrair dados
@@ -18,22 +18,29 @@ if response.status_code == 200:
     # Encontrar todos os campos com a classe 'field-get_info_principal'
     info_fields = soup.find_all('td', class_='field-get_info_principal')
     
+    # Remove arquivo antes de começar
+    filename = 'dados.txt'
+    if os.path.exists(filename):
+        os.remove(filename)
+
     # Abrir um arquivo de texto para salvar os nomes e e-mails
-    with open('dados.txt', 'w') as file:
-        file.write('nome,matricula,email\n')
+    with open(filename, 'w') as file:
         for field in info_fields:
             # Extrair o nome
             full_text = field.find('dd', class_='negrito').get_text(strip=True)
             name, id = full_text.rsplit(' ', 1)
             id = id.replace('(', '').replace(')', '')
-            
+
             # Extrair o e-mail
             email_tag = field.find('a', href=True)
             email = email_tag.get_text(strip=True) if email_tag else "E-mail não encontrado"
             
+            # Tamanho do preenchimento da linha
+            width = 50 - len(name)
+
             # Escrever no arquivo
-            file.write(f'{name},{id},{email}\n')
+            file.write(f'{name},{' ' * (48 - len(name))}{id},{' ' * (16 - len(id))}{email}\n')
     
-    print("Dados extraídos com sucesso. (dados.txt)")
+    print(f"Dados extraídos com sucesso. '{filename}'")
 else:
     print("Página não encontrada...")
